8cfebe5c236fee5b569d3bd56c3301003c0a238b
Ryan Chu <tianyuan.chu@gmail.com>
2019-03-23 11:44:19 -0700

    done

---
+++ code/snapshot.js
@@ -0,0 +1,37 @@
+// Jest Snapshot v1, https://goo.gl/fbAQLP
+
+exports[`would boil if above 100 1`] = `
+<p>
+  The water would boil.
+</p>
+`;
+
+exports[`would boil if at 100 1`] = `
+<p>
+  The water would boil.
+</p>
+`;
+
+exports[`would not boil if NaN 1`] = `
+<p>
+  The water would not boil.
+</p>
+`;
+
+exports[`would not boil if below 20 1`] = `
+<p>
+  The water would not boil.
+</p>
+`;
+
+exports[`would not boil if negative -1000 1`] = `
+<p>
+  The water would not boil.
+</p>
+`;
+
+exports[`would not boil if no input 1`] = `
+<p>
+  The water would not boil.
+</p>
+`;
